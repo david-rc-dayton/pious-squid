df41d3d6b4577120538e46a1e8beb99c58ea03bf
David RC Dayton <david.rc.dayton@gmail.com>
2018-02-01 12:08:58 -0700

    added kepler->j2k conversion

--- src/coordinates/keplerian.ts
+++ src/coordinates/keplerian.ts
@@ -1,4 +1,6 @@
 import { Epoch } from "../epoch";
+import { Vector } from "../vector";
+import { J2000 } from "./j2000";
 
 export class Keplerian {
     public epoch: Epoch;
@@ -18,5 +20,18 @@ export class Keplerian {
         this.o = o;
         this.w = w;
         this.v = v;
+    }
+
+    public toJ2K(): J2000 {
+        const rPqw = new Vector([
+            Math.cos(this.v), Math.sin(this.v), 0,
+        ]).scale((this.a * (1 - Math.pow(this.e, 2)))
+            / (1 + this.e * Math.cos(this.v)));
+        const vPqw = new Vector([
+            -Math.sin(this.v), this.e + Math.cos(this.v), 0,
+        ]);
+        const rJ2k = rPqw.rot3(-this.w).rot1(-this.i).rot3(-this.o);
+        const vJ2k = vPqw.rot3(-this.w).rot1(-this.i).rot3(-this.o);
+        return new J2000(this.epoch, rJ2k, vJ2k);
     }
 }
--- src/test/test-coordinates.ts
+++ src/test/test-coordinates.ts
@@ -1,11 +1,22 @@
 import * as assert from "assert";
-import { RAD2DEG } from "../constants";
+import { DEG2RAD, RAD2DEG } from "../constants";
 import { J2000 } from "../coordinates/j2000";
+import { Keplerian } from "../coordinates/keplerian";
 import { Epoch } from "../epoch";
 import { Vector } from "../vector";
 
 const TEST_J2K = new J2000(
     new Epoch(0), new Vector([8228, 389, 6888]), new Vector([-0.7, 6.6, -0.6]),
+);
+
+const TEST_KEPLER = new Keplerian(
+    new Epoch(0),
+    13360.642770119148,
+    0.22049791840816513,
+    DEG2RAD * 39.93754927254844,
+    DEG2RAD * 269.85555147445865,
+    DEG2RAD * 125.72438198841463,
+    DEG2RAD * 326.46253415447933,
 );
 
 describe("J2000", () => {
@@ -19,6 +30,18 @@ describe("J2000", () => {
             assert.equal(o * RAD2DEG, 269.85555147445865);
             assert.equal(w * RAD2DEG, 125.72438198841463);
             assert.equal(v * RAD2DEG, 326.46253415447933);
+        });
+    });
+});
+
+describe("Keplerian", () => {
+    describe("#.toJ2K", () => {
+        it("should convert to J2000 cartesian coordinates", () => {
+            const { epoch, position, velocity } = TEST_KEPLER.toJ2K();
+            assert.equal(epoch.epoch, TEST_KEPLER.epoch.epoch);
+            assert.deepEqual(position.state, [
+                8228, 389.0000000000039, 6888.000000000001,
+            ]);
         });
     });
 });
