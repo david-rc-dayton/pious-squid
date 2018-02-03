f79dfeb5b9d781e5835a2c2a2f8e53ff1a538580
David RC Dayton <david.rc.dayton@gmail.com>
2018-02-03 10:30:29 -0700

    added docs to operations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+import * as operationsImport from "./operations";
+export const operations = operationsImport;
+
 export { Vector } from "./vector";
--- src/operations.ts
+++ src/operations.ts
@@ -1,3 +1,7 @@
+/**
+ * Calculate the factorial of a number.
+ * @param n a positive integer
+ */
 export function factorial(n: number): number {
     let output = 1;
     for (let i = n; i > 1; i--) {
@@ -6,6 +10,11 @@ export function factorial(n: number): number {
     return output;
 }
 
+/**
+ * Evaluate a polynomial.
+ * @param x variable
+ * @param coeffs coefficients, from lowest to highest starting at exponent zero
+ */
 export function evalPoly(x: number, coeffs: number[]) {
     let output = 0;
     for (let n = 0; n < coeffs.length; n++) {
@@ -14,6 +23,10 @@ export function evalPoly(x: number, coeffs: number[]) {
     return output;
 }
 
+/**
+ * Return the sign of the number, 1 if positive, -1 if negative, 0 if zero.
+ * @param n a number
+ */
 export function sign(n: number): number {
     if (n < 0) { return -1; }
     if (n > 0) { return 1; }
