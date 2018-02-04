bfbf2844f7bc965d0338fbd81144eb6b9b7ef442
David RC Dayton <david.rc.dayton@gmail.com>
2018-02-04 10:18:09 -0700

    refactored module exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import * as bodiesImport from "./bodies";
-export const bodies = bodiesImport;
+import * as bodies from "./bodies";
+export { bodies };
 
-import * as constantsImport from "./constants";
-export const constants = constantsImport;
+import * as constants from "./constants";
+export { constants };
 
 export { EarthCenteredFixed } from "./coordinates/earth-centered-fixed";
 
@@ -10,8 +10,8 @@ export { EarthCenteredInertial } from "./coordinates/earth-centered-inertial";
 
 export { Epoch } from "./epoch";
 
-import * as forcesImport from "./forces";
-export const forces = forcesImport;
+import * as forces from "./forces";
+export { forces };
 
 export { Geodetic } from "./coordinates/geodetic";
 
@@ -21,8 +21,8 @@ export { Keplerian } from "./coordinates/keplerian";
 
 export { LookAngle } from "./coordinates/look-angle";
 
-import * as operationsImport from "./operations";
-export const operations = operationsImport;
+import * as operations from "./operations";
+export { operations };
 
 export { RungeKutta4 } from "./propagators/runge-kutta-4";
 
