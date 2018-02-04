8fbbbcae7e9f3a77ac12d32698d7d2db976fb519
David RC Dayton <david.rc.dayton@gmail.com>
2018-02-03 19:07:56 -0700

    removed unnecessary test case

--- src/test/test-propagators.ts
+++ src/test/test-propagators.ts
@@ -12,9 +12,6 @@ const GEO_STATE_1 = [
     new J2000(Date.UTC(2017, 5, 27, 19, 6, 33, 584),
         -4.13604755301e4, -7.62935881401e3, 3.68034831979e3,
         5.50737740226e-1, -3.01903793887e0, -1.06463779436e-1),
-    new J2000(Date.UTC(2017, 5, 28, 1, 6, 33, 584),
-        7.67012525578e3, -4.13874189375e4, -1.47082825564e3,
-        3.01260938022e0, 5.71966398298e-1, -2.67793178113e-1),
     new J2000(Date.UTC(2017, 5, 28, 7, 6, 33, 584),
         4.11526359989e4, 8.14874780238e3, -3.65210292647e3,
         -5.84795155962e-1, 3.02059500637e0, 1.09651877768e-1),
@@ -22,36 +19,21 @@ const GEO_STATE_1 = [
 
 describe("RungeKutta4", () => {
     describe("#.propagate()", () => {
-        it("should have an error less than 50 meters over 6 hours", () => {
-            for (let i = 0; i < GEO_STATE_1.length - 1; i++) {
-                const rk4 = new RungeKutta4(GEO_STATE_1[i]);
-                const { position } = rk4.propagate(
-                    GEO_STATE_1[i + 1].epoch.toMillis(),
-                );
-                const dist = position.distance(
-                    GEO_STATE_1[i + 1].position,
-                ) * 1000;
-                assert.ok(dist < 50, `Distance: ${dist.toFixed(2)} meters`);
-            }
+        const [GEO_0HR, GEO_6HR, GEO_12HR, GEO_24HR] = GEO_STATE_1;
+        const rk4 = new RungeKutta4(GEO_0HR, 300);
+        it("should have an error less than 50 meters after 6 hours", () => {
+            const { position } = rk4.propagate(GEO_6HR.epoch.toMillis());
+            const dist = position.distance(GEO_6HR.position) * 1000;
+            assert.ok(dist < 50, `Distance: ${dist.toFixed(2)} meters`);
         });
-        it("should have an error less than 100 meters over 12 hours", () => {
-            for (let i = 0; i < GEO_STATE_1.length - 2; i += 2) {
-                const rk4 = new RungeKutta4(GEO_STATE_1[i]);
-                const { position } = rk4.propagate(
-                    GEO_STATE_1[i + 2].epoch.toMillis(),
-                );
-                const dist = position.distance(
-                    GEO_STATE_1[i + 2].position,
-                ) * 1000;
-                assert.ok(dist < 100, `Distance: ${dist.toFixed(2)} meters`);
-            }
+        it("should have an error less than 100 meters after 12 hours", () => {
+            const { position } = rk4.propagate(GEO_12HR.epoch.toMillis());
+            const dist = position.distance(GEO_12HR.position) * 1000;
+            assert.ok(dist < 100, `Distance: ${dist.toFixed(2)} meters`);
         });
-        it("should have an error less than 200 meters over 24 hours", () => {
-            const rk4 = new RungeKutta4(GEO_STATE_1[0]);
-            const { position } = rk4.propagate(
-                GEO_STATE_1[4].epoch.toMillis(),
-            );
-            const dist = position.distance(GEO_STATE_1[4].position) * 1000;
+        it("should have an error less than 200 meters after 24 hours", () => {
+            const { position } = rk4.propagate(GEO_24HR.epoch.toMillis());
+            const dist = position.distance(GEO_24HR.position) * 1000;
             assert.ok(dist < 200, `Distance: ${dist.toFixed(2)} meters`);
         });
     });
