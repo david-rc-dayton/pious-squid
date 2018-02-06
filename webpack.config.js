d89ea101e6a86ad5bebb49692411f9f254abd220
David RC Dayton <david.rc.dayton@gmail.com>
2018-02-06 15:14:13 -0700

    start prepping for release

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: {
-        "unholy-octopus": path.join(__dirname, 'dist', 'index.js'),
-        "unholy-octopus.min": path.join(__dirname, 'dist', 'index.js'),
+        "pious-squid": path.join(__dirname, 'dist', 'index.js'),
+        "pious-squid.min": path.join(__dirname, 'dist', 'index.js'),
     },
     output: {
         path: path.join(__dirname, 'bundle'),
         filename: '[name].js',
-        library: 'UnholyOctopus'
+        library: 'PiousSquid'
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
