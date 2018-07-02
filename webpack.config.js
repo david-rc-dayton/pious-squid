9d5e675d096342d7d6396e7edf8e29d74a881984
David RC Dayton <david.rc.dayton@gmail.com>
2018-07-02 11:58:13 -0600

    fix webpack deprecations

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    entry: {
-        "pious-squid": path.join(__dirname, 'dist', 'index.js'),
-        "pious-squid.min": path.join(__dirname, 'dist', 'index.js'),
-    },
-    output: {
-        path: path.join(__dirname, 'bundle'),
-        filename: '[name].js',
-        library: 'PiousSquid'
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            include: /\.min\.js$/,
-            compress: true,
-            mangle: true
-        })
+  mode: "production",
+  entry: {
+    "pious-squid": path.join(__dirname, "dist", "index.js"),
+    "pious-squid.min": path.join(__dirname, "dist", "index.js")
+  },
+  output: {
+    path: path.join(__dirname, "bundle"),
+    filename: "[name].js",
+    library: "PiousSquid"
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        include: /\.min\.js$/
+      })
     ]
-}
+  }
+};
