2e39c63c5a342a180e26b4d984c6ed58df937fd5
David RC Dayton <david.rc.dayton@gmail.com>
2019-02-02 11:59:16 -0700

    use 1d array in spherical harmonics to reduce ram use

--- src/math/spherical-harmonics.ts
+++ src/math/spherical-harmonics.ts
@@ -3,7 +3,7 @@ export class SphericalHarmonics {
   /** dimension */
   private d: number;
   /** associated legendre polynomial table */
-  private P: number[][];
+  private P: number[];
 
   /**
    * Create a new SphericalHarmonics object.
@@ -15,6 +15,10 @@ export class SphericalHarmonics {
     this.P = [];
   }
 
+  private index(l: number, m: number) {
+    return (l * l + l) / 2 + m;
+  }
+
   /**
    * Fetch the associated legendre polynomial from the provided index.
    *
@@ -22,18 +26,17 @@ export class SphericalHarmonics {
    * @param m m-index
    */
   public getP(l: number, m: number) {
-    return this.P[l][m] || 0;
+    if (m > l) {
+      return 0;
+    }
+    return this.P[this.index(l, m)] || 0;
   }
 
   /** Reset the polynomial table to zeroes. */
   private clearTable() {
     this.P = [];
-    for (let l = 0; l <= this.d; l++) {
-      const temp = [];
-      for (let m = 0; m <= l + 1; m++) {
-        temp.push(0);
-      }
-      this.P.push(temp);
+    for (let i = 0; i <= this.index(this.d, this.d); i++) {
+      this.P.push(0);
     }
   }
 
@@ -47,22 +50,21 @@ export class SphericalHarmonics {
     const { d, P } = this;
     const sPhi = Math.sin(phi);
     const cPhi = Math.cos(phi);
-    P[0][0] = 1.0;
-    P[0][1] = 0.0;
-    P[1][0] = sPhi;
-    P[1][1] = cPhi;
+    P[this.index(0, 0)] = 1;
+    P[this.index(1, 0)] = sPhi;
+    P[this.index(1, 1)] = cPhi;
     for (let l = 2; l <= d; l++) {
       for (let m = 0; m <= l; m++) {
         if (l >= 2 && m == 0) {
-          P[l][0] =
+          P[this.index(l, 0)] =
             ((2 * l - 1) * sPhi * this.getP(l - 1, 0) -
               (l - 1) * this.getP(l - 2, 0)) /
             l;
         } else if (m != 0 && m < l) {
-          P[l][m] =
+          P[this.index(l, m)] =
             this.getP(l - 2, m) + (2 * l - 1) * cPhi * this.getP(l - 1, m - 1);
         } else if (l == m && l != 0) {
-          P[l][l] = (2 * l - 1) * cPhi * this.getP(l - 1, l - 1);
+          P[this.index(l, l)] = (2 * l - 1) * cPhi * this.getP(l - 1, l - 1);
         }
       }
     }
